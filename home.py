29b00b60a14dae1bf9b843e8ed1429a781360b5f
sotnasnonnel <90616389+sotnasnonnel@users.noreply.github.com>
2024-01-13 06:07:44 -0300

    Add files via upload

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st  
+
+
+st.sidebar.markdown('Desenvolvido por Lennon')
+
+st.markdown('# Bem-Vindo ao Analisador de vendas')
+
+st.divider()
+
+st.markdown(
+    '''
+        Esse Projeto foi desenvolvido como projeto final do curso ***Python***.
+
+    '''
+)
+
